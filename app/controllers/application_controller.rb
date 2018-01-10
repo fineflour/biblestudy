7faa1514c54c8632679bcd0c91a786d59d484c3c
Ahilan Beyond <ahilan@beyondfinity.com>
2018-01-10 12:52:24 +0530

    Add 15 views according to the topics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,60 @@ class ApplicationController < ActionController::Base
     render layout: 'application'
   end
 
+  def god
+    render layout: 'application'
+  end
+
+  def the_economy_of_god
+    render layout: 'application'
+  end
+
+  def christ
+    render layout: 'application'
+  end
+
+  def the_spirit
+    render layout: 'application'
+  end
+
+  def eternal_life
+    render layout: 'application'
+  end
+
+  def divine_truth_reality
+    render layout: 'application'
+  end
+
+  def man
+    render layout: 'application'
+  end
+
+  def the_gospel
+    render layout: 'application'
+  end
+
+  def salvation
+    render layout: 'application'
+  end
+
+  def christian_experience
+    render layout: 'application'
+  end
+
+  def the_believers
+    render layout: 'application'
+  end
+
+  def the_church_the_body_of_christ
+    render layout: 'application'
+  end
+
+  def the_kingdom
+    render layout: 'application'
+  end
+
+  def the_new_jerusalem
+    render layout: 'application'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,20 @@ Rails.application.routes.draw do
   get 'home/pages/:file' => 'home#pages'
   get 'home/content'
   get 'home/the_word_of_god'
+  get 'home/god'
+  get 'home/the_economy_of_god'
+  get 'home/christ'
+  get 'home/the_spirit'
+  get 'home/eternal_life'
+  get 'home/divine_truth_reality'
+  get 'home/man'
+  get 'home/the_gospel'
+  get 'home/salvation'
+  get 'home/christian_experience'
+  get 'home/the_believers'
+  get 'home/the_church_the_body_of_christ'
+  get 'home/the_kingdom'
+  get 'home/the_new_jerusalem'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
