8fcea170c79092834c5dee3c84dbf1377d7980b5
Bharathi Beyond <bharathi@beyondfinity.com>
2018-01-19 12:30:00 +0530

    Fix merge conflict

--- app/helpers/phones_helper.rb
+++ app/helpers/phones_helper.rb
@@ -1,5 +1,5 @@
 module PhonesHelper
-  def panel_link path: nil, text: nil
-    render 'application/phone/panel_link', path: path, text: text
+  def panel_link path: nil, text: nil, icon: nil
+    render 'application/phone/panel_link', path: path, text: text, icon_name: icon
   end
 end
